9f2a8e826b4dcf8244aca07f65195f45d2b2ccc2
Sand Jessica <jessica.sand.mjm@gmail.com>
2021-07-18 17:14:31 +0200

    Implementation of CRUD methods for GenreController + templates

--- src/Controller/Admin/GenreController.php
+++ src/Controller/Admin/GenreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +15,42 @@ class GenreController extends AbstractController
     /**
      * @Route("/list", name="list")
      */
-    public function list(): Response
+    public function list(GenreRepository $genreRepository): Response
     {
         return $this->render('admin/genre/list.html.twig', [
-            'controller_name' => 'GenreController',
+            'genres' => $genreRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(): Response
+    {
+        return $this->render('admin/genre/add.html.twig');
+    }
+
+    /**
+     * @Route("/show", name="show")
+     */
+    public function show(): Response
+    {
+        return $this->render('admin/genre/show.html.twig');
+    }
+
+    /**
+     * @Route("/edit", name="edit")
+     */
+    public function edit(): Response
+    {
+        return $this->render('admin/genre/edit.html.twig');
+    }
+
+    /**
+     * @Route("/delete", name="delete")
+     */
+    public function delete(): Response
+    {
+        return $this->render('admin/genre/delete.html.twig');
     }
 }
