39ec5d5f67fbb1c583c744dcf676a5a50bd4faed
Carlos <carlos.or.pro@gmail.com>
2021-07-26 10:08:57 +0200

    Setting up voters and roles in Backoffice + Adding a new entity (app_user)

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -8,7 +8,6 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -50,36 +49,6 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/add", name="add")
-     * @return void
-     */
-    public function add(Request $request, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $user = new User();
-
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
-
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($user);
-            $em->flush();
-
-            // Flash message
-            $this->addFlash('info', 'L\'utilisateur ' . $user->getPseudo() . ' a bien été créée');
-            return $this->redirectToRoute('admin_user_list');
-        }
-
-        return $this->render('admin/user/add.html.twig', [
-            'form' => $form->createView(),
-        ]);  
-    }
-
-    /**
      * @Route("/{id}/status", name="status_change")
      *
      * Function for change the status of the User in the DataBase
@@ -91,12 +60,12 @@ class UserController extends AbstractController
             $user->setStatus(0);
             $em->flush();
 
-            return $this->redirectToRoute('admin_user_list', ['id' => $user->getId()]);
+            return $this->redirectToRoute('admin_user_list');
         }else{
             $user->setStatus(1);
             $em->flush();
 
-            return $this->redirectToRoute('admin_user_list', ['id' => $user->getId()]);
+            return $this->redirectToRoute('admin_user_list');
         }
     }
 
