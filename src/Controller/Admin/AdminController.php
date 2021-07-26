e43e7ed07b0da4bd1ee5a0ec0ad8cb7f4fc52621
Sand Jessica <jessica.sand.mjm@gmail.com>
2021-07-26 11:00:46 +0200

    Modification role admin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -13,9 +13,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
- * @Route("/", name="admin_")
+ * @Route("", name="admin_")
+ * @IsGranted("ROLE_ADMIN")
  */
 class AdminController extends AbstractController
 {
@@ -33,6 +35,8 @@ class AdminController extends AbstractController
     /**
      * @Route("/add", name="add")
      * @return void
+     * 
+     * @IsGranted("ROLE_SUPER_ADMIN")
      */
     public function add(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
