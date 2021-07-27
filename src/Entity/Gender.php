8cab492ca1279006eab218703c069588abe9d13c
Sand Jessica <jessica.sand.mjm@gmail.com>
2021-07-27 16:20:12 +0200

    Use added in entities for groups annotation and assert

--- src/Entity/Gender.php
+++ src/Entity/Gender.php
@@ -7,6 +7,8 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=GenderRepository::class)
